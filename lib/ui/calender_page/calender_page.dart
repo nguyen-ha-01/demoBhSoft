cb473841f88de598f589a8f754b3bbd70a1cc6b3
nguyen-ha-01 <aa12nguyenha@gmail.com>
2024-06-05 14:20:42 +0700

    add common task widget and common assets

---
+++ lib/ui/calender_page/calender_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalenderPage extends StatefulWidget {
+  const CalenderPage({super.key});
+  @override
+  _CalenderPageState createState() => _CalenderPageState();
+}
+
+class _CalenderPageState extends State<CalenderPage> {
+  _CalenderPageState();
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/focus_page/focus_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FocusPage extends StatefulWidget {
+  const FocusPage({super.key});
+  @override
+  _FocusPageState createState() => _FocusPageState();
+}
+
+class _FocusPageState extends State<FocusPage> {
+  _FocusPageState();
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
